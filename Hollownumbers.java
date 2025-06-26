ecfe5fc9cd603efc2eeee5091d7cc9a9c363c473
Saivanshika10 <saivanshika11@gmail.com>
2025-06-26 13:07:03 +0530

    Update Hollownumbers.java

--- Hollownumbers.java
+++ Hollownumbers.java
@@ -1,13 +1,13 @@
 public class Hollownumbers {
     public static void main(String[] args) {
-        int rows = 5;  // You can change the number of rows
+        int rows = 5;  
 
         for (int i = 1; i <= rows; i++) {
-            // Print numbers from 1 to i
+            
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
-            System.out.println(); // Move to next line
+            System.out.println(); 
         }
     }
 }
