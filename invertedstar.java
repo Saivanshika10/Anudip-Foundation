939fc045e659a1ccfec5a83688f6d716ddf7ce56
Saivanshika10 <saivanshika11@gmail.com>
2025-06-26 13:07:46 +0530

    Update invertedstar.java

--- invertedstar.java
+++ invertedstar.java
@@ -1,13 +1,13 @@
 public class invertedstar {
     public static void main(String[] args) {
-        int rows = 5; // You can change the number of rows
+        int rows = 5;
 
         for (int i = rows; i >= 1; i--) {
-            // Print stars for each row
+           
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
-            System.out.println(); // Move to next line
+            System.out.println(); 
         }
     }
 }
